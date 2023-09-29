ee283d7729aa67348941b67ce61db1ba59f4cf56
jossjic <jossjic_03@hotmail.com>
2023-09-29 12:49:05 -0600

    Pathfinding, matrix

--- back/incinerador.py
+++ back/incinerador.py
@@ -8,73 +8,64 @@ from mesa.visualization.UserParam import Slider
 from mesa.visualization.modules import ChartModule
 from mesa.datacollection import DataCollector
 import numpy as np
+from pathfinding.core.grid import Grid
+from pathfinding.finder.a_star import AStarFinder
 
 
 import heapq # Librería para el método de búsquda del camino más corto A*
 
 steps = 0
 
-class Bot(Agent):
+
+
+class Agentes(Agent):
     w_trash=False
     def __init__(self, model, pos, incineradorAgent):
         super().__init__(model.next_id(), model)
         self.pos = pos
-        self.type = "Bot"
+        self.type = "robot"
         self.incineradorAgent = incineradorAgent
-        print(self.incineradorAgent)
 
     def step(self):
+        global globalMatrix
         global steps
-        
-        if self.w_trash==False:
+        if not self.w_trash:
             new_pos = self.pos + np.array([self.random.randrange(-1, 2, 1),self.random.randrange(-1,2,1)])
-            while (self.model.grid.out_of_bounds(new_pos) or ((new_pos[0]==((self.model.grid.width-2)//2)) and (new_pos[1]==((self.model.grid.height-2)//2)))):
+            while (self.model.grid.out_of_bounds(new_pos) or (new_pos[0]==self.incineradorAgent.pos[0] and new_pos[1]==self.incineradorAgent.pos[1])):
                         new_pos = self.pos + np.array([self.random.randrange(-1, 2, 1),self.random.randrange(-1,2,1)])
             self.model.grid.move_agent(self, new_pos)
             if steps > 3:
                 self.incineradorAgent.condition = self.incineradorAgent.OFF
                 steps = 0
-        else:
-                new_pos = self.pos                
-                if (self.pos[0] != ((self.model.grid.width-2)//2)) or (self.pos[1] != ((self.model.grid.height-2)//2)):
-                    # Inicializar new_pos con la posición actual
+        else:     
+            new_pos = self.pos    
+            print(globalMatrix)
+            if (self.pos[0] != self.incineradorAgent.pos[0]) or (self.pos[1] != self.incineradorAgent.pos[1]):
+                grid = Grid(matrix=globalMatrix.tolist())
+                finder = AStarFinder()
+                (r1, r2) = self.incineradorAgent.pos 
+                (g1, g2) = self.pos
+                incinerador_pos = grid.node(r1, r2)
+                self_pos = grid.node(g1, g2)
+                path, _ = finder.find_path(self_pos, incinerador_pos, grid)
+                if len(path) > 1:
+                    new_pos = path[1]
+                    print("si se puede")
                     
-                    if self.pos[0] < ((self.model.grid.width - 2) // 2):
-                        new_pos = new_pos + np.array([1, 0])
-                    elif self.pos[0] > ((self.model.grid.width - 2) // 2):
-                        new_pos = new_pos + np.array([-1, 0])
-                    
-                    if self.pos[1] < ((self.model.grid.height - 2) // 2):
-                        new_pos = new_pos + np.array([0, 1])
-                    elif self.pos[1] > ((self.model.grid.height - 2) // 2):
-                        new_pos = new_pos + np.array([0, -1])
-                            
-                    elif self.pos[0] == ((self.model.grid.width-2)//2):
-                        if self.pos[1] < ((self.model.grid.height-2)//2):
-                            new_pos = self.pos + np.array([0,1])
-                        elif self.pos[1] > ((self.model.grid.height-2)//2):
-                            new_pos = self.pos + np.array([0,-1])
-                    
-                    elif self.pos[1] == ((self.model.grid.height-2)//2):
-                        if self.pos[0] < ((self.model.grid.width-2)//2):
-                            new_pos = self.pos + np.array([1,0])
-                        elif self.pos[0] > ((self.model.grid.width-2)//2):
-                            new_pos = self.pos + np.array([-1,0])
-                    
-                else:
-                    self.w_trash=False
-                    self.incineradorAgent.condition = self.incineradorAgent.ON
-                
-                if (self.incineradorAgent.condition == self.incineradorAgent.ON) and ((new_pos[0]==((self.model.grid.width-2)//2)) and (new_pos[1]==((self.model.grid.height-2)//2))):
-                    self.model.grid.move_agent(self, self.pos)
-                else:
-                    self.model.grid.move_agent(self, new_pos)
+                        
+            else:
+                self.w_trash=False
+                self.incineradorAgent.condition = self.incineradorAgent.ON
+            incineradorOn = self.incineradorAgent.condition == self.incineradorAgent.ON and new_pos[0]==self.incineradorAgent.pos[0] and new_pos[1]==self.incineradorAgent.pos[1]
+            if not incineradorOn:
+                self.model.grid.move_agent(self, new_pos)
     
                 
                 
         for element in self.model.grid.get_cell_list_contents([self.pos]):
             if type(element) == Basura and element.condition == element.UNCOLLECT and not self.w_trash:
                 element.condition = element.COLLECT
+                globalMatrix[self.pos[0]][self.pos[1]]=1
                 self.w_trash=True
 
 
@@ -84,7 +75,7 @@ class Incinerador(Agent):
     def __init__(self, model, pos):
         super().__init__(model.next_id(), model)
         self.pos = pos
-        self.type = "Incinerador"
+        self.type = "incinerador"
         self.condition= self.OFF
         self.timer = 0
         
@@ -106,15 +97,17 @@ class Basura(Agent):
     def __init__(self, model):
         super().__init__(model.next_id(), model)
         self.condition = self.UNCOLLECT
-        self.type = "Basura"
+        self.type = "basura"
         
 
 class   Sala(Model):
+    
     def getGridSize(self):
         return self.grid.width, self.grid.height
     
-    def __init__(self, density=0.45, grid_size=False, max_steps=1000, conta = 0):
+    def __init__(self, density=0.2, grid_size=False, max_steps=100000, conta = 0):
         super().__init__()
+        global globalMatrix
         
         if grid_size:
             self.grid = MultiGrid(51, 51, torus=False)
@@ -122,45 +115,49 @@ class   Sala(Model):
             self.grid = MultiGrid(21, 21, torus=False)
         self.schedule = RandomActivation(self)
         self.max_steps = max_steps
+        self.matrix = np.ones((self.grid.width, self.grid.height), dtype=np.int8)
        
         
-        print(self.grid.width, self.grid.height)
 
         incinerador = Incinerador(self, ((self.grid.width-2)//2, (self.grid.height-2)//2))  
         self.grid.place_agent(incinerador, incinerador.pos)
         self.schedule.add(incinerador)
         
-        robot1 = Bot(self, (0, 0), incinerador)
+        for _,(x,y) in self.grid.coord_iter():  
+            if self.random.random() < density and (x,y)!=(incinerador.pos[0],incinerador.pos[1]) and (x,y)!=(0,0) and (x,y)!=(0,self.grid.height-1) and (x,y)!=(self.grid.width-1,0) and (x,y)!=(self.grid.width-1,self.grid.height-1):
+                basura = Basura(self)
+                self.grid.place_agent(basura, (x,y))
+                self.schedule.add(basura)
+                self.matrix[x][y]=0
+        self.matrix=np.rot90(self.matrix, k=1)
+        globalMatrix = self.matrix
+        
+        
+        robot1 = Agentes(self, (0, 0), incinerador)
         self.grid.place_agent(robot1, robot1.pos)
         self.schedule.add(robot1)
                 
 
-        robot2 = Bot(self, (0, self.grid.height - 1), incinerador)  
+        robot2 = Agentes(self, (0, self.grid.height - 1), incinerador)  
         self.grid.place_agent(robot2, robot2.pos)
         self.schedule.add(robot2)
                 
 
-        robot3 = Bot(self, (self.grid.width-1, 0), incinerador)  
+        robot3 = Agentes(self, (self.grid.width-1, 0), incinerador)  
         self.grid.place_agent(robot3, robot3.pos)
         self.schedule.add(robot3)
                 
 
-        robot4 = Bot(self, (self.grid.width-1, self.grid.height-1), incinerador)  
+        robot4 = Agentes(self, (self.grid.width-1, self.grid.height-1), incinerador)  
         self.grid.place_agent(robot4, robot4.pos)
         self.schedule.add(robot4)
                 
             
-        robot5 = Bot(self, ((self.grid.width-1)//2, (self.grid.height-1)//2), incinerador)  
+        robot5 = Agentes(self, ((self.grid.width-1)//2, (self.grid.height-1)//2), incinerador)  
         self.grid.place_agent(robot5, robot5.pos)
         self.schedule.add(robot5)
-
-
-        for _,(x,y) in self.grid.coord_iter():  
-            if self.random.random() < density:
-                basura = Basura(self)
-                self.grid.place_agent(basura, (x,y))
-                self.schedule.add(basura)
-
+        
+        
         self.datacollector = DataCollector({"Percent burned": lambda m: self.count_type(m, Basura.COLLECT) / len(self.schedule.agents)})
                         
              
@@ -178,5 +175,3 @@ class   Sala(Model):
             self.schedule.step()
             steps += 1
             print(steps)
-            
-
